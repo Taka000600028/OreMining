f45e87c80ca8f684aa3b8f242a88bf3306e2b799
Taka000600028 <tkh2270@gmail.com>
2025-06-28 04:22:31 +0900

    Add files via upload

---
+++ GameStart/build/classes/java/main/plugin/gameStart/command/BaseCommand.java
@@ -0,0 +1,44 @@
+package plugin.gameStart.command;
+
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+
+/**
+ * コマンドを実行して動かすプラグイン処理の基底クラスです。
+ */
+
+public abstract class BaseCommand implements CommandExecutor {
+
+  @Override
+  public boolean onCommand(CommandSender commandSender, Command command, String s,
+      String[] strings) {
+    if (commandSender instanceof Player player) {
+      return onExecutePlayerCommand(player,commandSender,s,strings);
+    } else {
+      return onExecuteNPCCommand(commandSender);
+    }
+  }
+
+  /**
+   * コマンド実行者がプレイヤーだった場合に実行します。
+   *
+   * @param player　コマンドを実行したプレイヤー
+   * @return 処理の実行有無
+   */
+  public abstract boolean onExecutePlayerCommand(Player player);
+
+  public abstract boolean onExecutePlayerCommand(Player player, CommandSender commandSender,
+      String s,
+      String[] strings);
+
+  /**
+   * コマンド実行者がプレイヤー以外だった場合に実行します。
+   *
+   * @param commandSender　コマンド実行者
+   * @return 処理の実行有無
+   */
+  public abstract boolean onExecuteNPCCommand(CommandSender commandSender);
+
+}
---
+++ GameStart/src/main/java/plugin/gameStart/mapper/data/PlayerScore.java
@@ -0,0 +1,20 @@
+package plugin.gameStart.mapper.data;
+
+import lombok.Getter;
+import lombok.Setter;
+
+/**
+ *プレイヤーのスコア情報を扱うオブジェクト。
+ *DBに存在するテーブルと連動する。
+ */
+@Getter
+@Setter
+public class PlayerScore {
+
+  private int id;
+  private String playerName;
+  private int score;
+  private String registered_at;
+
+
+}
