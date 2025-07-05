05334579d8dde7ec5dcfeaadbc8d114adba37f18
Taka000600028 <tkh2270@gmail.com>
2025-07-06 06:05:39 +0900

    Add files via upload

--- GameStart/src/main/java/plugin/gameStart/command/BaseCommand.java
+++ GameStart/src/main/java/plugin/gameStart/command/BaseCommand.java
@@ -8,13 +8,11 @@ import org.bukkit.entity.Player;
 /**
  * コマンドを実行して動かすプラグイン処理の基底クラスです。
  */
-
 public abstract class BaseCommand implements CommandExecutor {
 
   @Override
   public boolean onCommand(CommandSender commandSender, Command command, String s,
       String[] strings) {
-
     if (commandSender instanceof Player player) {
       return onExecutePlayerCommand(player, commandSender, s, strings);
     } else {
@@ -24,20 +22,20 @@ public abstract class BaseCommand implements CommandExecutor {
 
   /**
    * コマンド実行者がプレイヤーだった場合に実行します。
-   *
-   * @param player 　コマンドを実行したプレイヤー
-   * @return 処理の実行有無
+   * @param player　コマンドを実行したプレイヤー
+   * @param commandSender　コマンド
+   * @param s　変数名
+   * @param strings　コマンド引数
+   * @return  処理の実行有無
    */
-
   public abstract boolean onExecutePlayerCommand(Player player, CommandSender commandSender,
-      String s,
-      String[] strings);
+      String s,String[] strings);
 
   /**
    * コマンド実行者がプレイヤー以外だった場合に実行します。
    *
    * @param commandSender 　コマンド実行者
-   * @return 処理の実行有無
+   * @return  処理の実行有無
    */
   public abstract boolean onExecuteNPCCommand(CommandSender commandSender);
 }
--- GameStart/src/main/java/plugin/gameStart/command/GameStartCommand.java
+++ GameStart/src/main/java/plugin/gameStart/command/GameStartCommand.java
@@ -23,16 +23,14 @@ import plugin.gameStart.Main;
 import plugin.gameStart.mapper.data.PlayerScore;
 
 /**
- * 制限時間内に出現するブロックを壊して、スコアを獲得するゲームを起動するコマンドです。
- * スコアはブロックの種類によって変わり、合計点数が変動します。
+ * 制限時間内に出現するブロックを壊して、スコアを獲得するゲームを起動するコマンドです。 スコアはブロックの種類によって変わり、合計点数が変動します。
  * 結果はプレイヤー名、点数、日時などで保存されます。
- **/
+ */
 public class GameStartCommand extends BaseCommand implements Listener {
 
   public static final int GAME_TIME = 40;
-  private final Main main;
-  private int gameTime = 40;
   private static final String LIST = "list";
+  private final Main main;
   private int currentScore = 0;
   private final PlayerScoreData playerScoreData = new PlayerScoreData();
   private final List<BlockData> originalAndesitesBlocks = new ArrayList<>();
@@ -41,26 +39,21 @@ public class GameStartCommand extends BaseCommand implements Listener {
     this.main = main;
   }
 
-   @Override
+
+  @Override
   public boolean onExecutePlayerCommand(Player player, CommandSender commandSender, String s,
       String[] strings) {
-
-    gameTime = GAME_TIME;
-    Main main = this.main;
-    currentScore = 0;
-
     // 最初の引数が「list」だったら、スコアを一覧で表示して処理を終了する。
     if (strings.length == 1 && LIST.equals(strings[0])) {
       sendPlayerScoreList(player);
       return true;
     }
 
+    Main main = this.main;
+    currentScore = 0;
+
     //　ゲーム開始前の装備を記録しておく。
     PlayerInventory inventory = player.getInventory();
-    ItemStack helmet = inventory.getHelmet();
-    ItemStack chestPlate = inventory.getChestplate();
-    ItemStack leggings = inventory.getLeggings();
-    ItemStack boots = inventory.getBoots();
     ItemStack itemInMainHand = inventory.getItemInMainHand();
 
     //ゲーム開始後、指定された場所に移動する。
@@ -91,20 +84,65 @@ public class GameStartCommand extends BaseCommand implements Listener {
     player.sendMessage("BLOCKを記録しました。");
 
     //コマンドを実行の間、装備する。
-    inventory.setHelmet(new ItemStack(Material.NETHERITE_HELMET));
-    inventory.setChestplate(new ItemStack(Material.NETHERITE_CHESTPLATE));
-    inventory.setLeggings(new ItemStack(Material.NETHERITE_LEGGINGS));
-    inventory.setBoots(new ItemStack(Material.NETHERITE_BOOTS));
     inventory.setItemInMainHand(new ItemStack(Material.NETHERITE_PICKAXE));
 
-    gamePlay(player, main, fromLocation, inventory, helmet, chestPlate, leggings, boots,
-        itemInMainHand);
+    gamePlay(player, main, fromLocation, inventory,itemInMainHand);
     return true;
   }
 
-  @Override
-  public boolean onExecuteNPCCommand(CommandSender commandSender) {
-    return false;
+  /**
+   * ゲームを実行します。規定時間内にブロックを壊すとスコアが加算されます。合計スコアを時間経過後に表示します。
+   * 持ち物はゲームを実行すると設定された持ち物に変更されます。
+   *
+   * @param player         　コマンドを実行したプレイヤー
+   * @param main           　クラス名
+   * @param fromLocation   　コマンドを実行する時にいる場所の情報
+   * @param inventory      　プレイヤーの持ち物
+   * @param itemInMainHand 　コマンドを実行する時に装着している武器
+   */
+  private void gamePlay(Player player, Main main, Location fromLocation, PlayerInventory inventory,
+      ItemStack itemInMainHand) {
+    new BukkitRunnable() {
+      int timeLeft = GAME_TIME;
+
+      @Override
+      public void run() {
+        if (timeLeft <= 20) {
+          cancel();
+
+          clearOreArea1(player.getWorld());
+          clearOreArea2(player.getWorld());
+
+          for (BlockData data : originalAndesitesBlocks) {
+            data.restore();
+          }
+          player.sendMessage("BLOCKを元に戻しました。");
+
+          int finalScore = currentScore;
+          player.sendTitle("ゲームが終了しました！",
+              player.getName() + "のスコアは" + finalScore + "点！",
+              30, 30, 30);
+
+          Bukkit.getScheduler().runTaskLater(main, () -> {
+            player.teleport(fromLocation);
+
+            //ゲーム開始前の装備に戻す。
+            inventory.setItemInMainHand(itemInMainHand);
+
+            player.sendMessage("元の位置に戻りました。");
+          }, 20 * 10);
+
+          playerScoreData.insert(
+              new PlayerScore(player.getName(), finalScore));
+          return;
+        }
+
+        spawnOre1(player.getWorld(), new SplittableRandom());
+        spawnOre2(player.getWorld(), new SplittableRandom());
+
+        timeLeft -= 50;
+      }
+    }.runTaskTimer(main, 0, 20 * 50);
   }
 
   @EventHandler
@@ -116,6 +154,11 @@ public class GameStartCommand extends BaseCommand implements Listener {
       currentScore += score;
       e.getPlayer().sendMessage("§a+" + score + "点！　現在のスコア：" + currentScore);
     }
+  }
+
+  @Override
+  public boolean onExecuteNPCCommand(CommandSender commandSender) {
+    return false;
   }
 
   /**
@@ -173,7 +216,12 @@ public class GameStartCommand extends BaseCommand implements Listener {
     }
   }
 
-  //1のエリアに4*4*4のブロックを出現させる。
+  /**
+   * 1つ目のエリアにブロックを出現させます。
+   *
+   * @param world　ブロックを出現させるワールド
+   * @param splittableRandom　出現するブロックのランダム性
+   */
   private static void spawnOre1(World world, SplittableRandom splittableRandom) {
     for (int a = 0; a <= 3; a++) {
       for (int b = 0; b <= 3; b++) {
@@ -187,19 +235,12 @@ public class GameStartCommand extends BaseCommand implements Listener {
     }
   }
 
-  //１のエリアの4*4*4のブロックを消去する。
-  private void clearOreArea1(World world) {
-    for (int a = 0; a <= 3; a++) {
-      for (int b = 0; b <= 3; b++) {
-        for (int c = 0; c <= 3; c++) {
-          Location location = new Location(world, 140 + a, 58 + b, -49 + c);
-          world.getBlockAt(location).setType(Material.AIR);
-        }
-      }
-    }
-  }
-
-  //２のエリアの4*4*4のブロックを出現させる。
+  /**
+   * 2つ目のエリアにブロックを出現させます。
+   *
+   * @param world　ブロックを出現させるワールド
+   * @param splittableRandom　出現させるブロックのランダム性
+   */
   private static void spawnOre2(World world, SplittableRandom splittableRandom) {
     for (int a = 0; a <= 3; a++) {
       for (int b = 0; b <= 3; b++) {
@@ -213,7 +254,27 @@ public class GameStartCommand extends BaseCommand implements Listener {
     }
   }
 
-  //２のエリアの4*4*4のブロックを消去する。
+  /**
+   * 1つ目のエリアに出現させたブロックを削除します。
+   *
+   * @param world　ブロックを出現させたワールド
+   */
+  private void clearOreArea1(World world) {
+    for (int a = 0; a <= 3; a++) {
+      for (int b = 0; b <= 3; b++) {
+        for (int c = 0; c <= 3; c++) {
+          Location location = new Location(world, 140 + a, 58 + b, -49 + c);
+          world.getBlockAt(location).setType(Material.AIR);
+        }
+      }
+    }
+  }
+
+  /**
+   * 2つ目のエリアに出現させたブロックを削除します。
+   *
+   * @param world　ブロックを出現させたワールド
+   */
   private void clearOreArea2(World world) {
     for (int a = 0; a <= 3; a++) {
       for (int b = 0; b <= 3; b++) {
@@ -223,69 +284,5 @@ public class GameStartCommand extends BaseCommand implements Listener {
         }
       }
     }
-  }
-
-  /**
-   * ゲームを実行します。規定時間内にブロックを壊すとスコアが加算されます。合計スコアを時間経過後に表示します。 持ち物はゲームを実行すると設定された持ち物に変更されます。
-   *
-   * @param player         　コマンドを実行したプレイヤー
-   * @param main           　クラス名
-   * @param fromLocation   　コマンドを実行する時にいる場所の情報
-   * @param inventory      　プレイヤーの持ち物
-   * @param helmet         　コマンドを実行する時に装着しているヘルメット
-   * @param chestPlate     　コマンドを実行する時に装着しているチェストプレート
-   * @param leggings       　コマンドを実行する時に装着しているレギンス
-   * @param boots          　コマンドを実行する時に装着しているブーツ
-   * @param itemInMainHand 　コマンドを実行する時に装着している武器
-   */
-  private void gamePlay(Player player, Main main, Location fromLocation, PlayerInventory inventory,
-      ItemStack helmet, ItemStack chestPlate, ItemStack leggings, ItemStack boots,
-      ItemStack itemInMainHand) {
-    new BukkitRunnable() {
-      int timeLeft = gameTime;
-
-      @Override
-      public void run() {
-        if (timeLeft <= 20) {
-          cancel();
-
-          clearOreArea1(player.getWorld());
-          clearOreArea2(player.getWorld());
-
-          for (BlockData data : originalAndesitesBlocks) {
-            data.restore();
-          }
-          player.sendMessage("BLOCKを元に戻しました。");
-
-          int finalScore = currentScore;
-          player.sendTitle("ゲームが終了しました！",
-              player.getName() + "のスコアは" + finalScore + "点！",
-              30, 30, 30);
-
-          Bukkit.getScheduler().runTaskLater(main, () -> {
-            player.teleport(fromLocation);
-
-            //ゲーム開始前の装備に戻す。
-            inventory.setHelmet(helmet);
-            inventory.setChestplate(chestPlate);
-            inventory.setLeggings(leggings);
-            inventory.setBoots(boots);
-            inventory.setItemInMainHand(itemInMainHand);
-
-            player.sendMessage("元の位置に戻りました。");
-          }, 20 * 10);
-
-          playerScoreData.insert(
-              new PlayerScore(player.getName(), finalScore));
-
-          return;
-        }
-
-        spawnOre1(player.getWorld(), new SplittableRandom());
-        spawnOre2(player.getWorld(), new SplittableRandom());
-
-        timeLeft -= 50;
-      }
-    }.runTaskTimer(main, 0, 20 * 50);
   }
 }
